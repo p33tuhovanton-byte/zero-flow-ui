31c55bd1accfbd5405b2f845f48d9b0c05f3d2d5
p33tuhovanton-byte <p33tuhov.anton@gmail.com>
2026-07-02 09:35:25 +0500

    Update main.go

--- example/main.go
+++ example/main.go
@@ -7,10 +7,99 @@ import (
 	"golang.org/x/mobile/event/size"
 	"golang.org/x/mobile/event/touch"
 	"golang.org/x/mobile/gl"
-
-  "core"
-  "scene"
 )
+
+type Object interface {
+	IdentifyClass() // Идеально пустая сигнатура
+}
+
+type ClassConsumer interface {
+	AcceptClassName() // Идеально пустая сигнатура
+}
+
+type Action interface {
+	Object
+	Execute()
+}
+
+type Bool interface {
+	Object
+	Select() Action
+}
+
+type EmptyAction struct{}
+
+func (ea EmptyAction) IdentifyClass() {}
+func (ea EmptyAction) Execute()       {}
+
+type Number interface {
+	Object
+	Next() Number
+	CheckEquality() Bool
+	CompareWithZero() Bool
+	CompareWithSuccessor() Bool
+	IsMultipleOfGrid() Bool
+}
+
+type Zero struct{ CompareTarget Number }
+
+func (z Zero) IdentifyClass()       {}
+func (z Zero) Class() string        { return "Zero" }
+func (z Zero) Next() Number         { return Successor{pred: z} }
+func (z Zero) CheckEquality() Bool  { return z.CompareTarget.CompareWithZero() }
+func (z Zero) CompareWithZero() Bool { return True{TrueBranch: EmptyAction{}, FalseBranch: EmptyAction{}} }
+func (z Zero) CompareWithSuccessor() Bool { return False{TrueBranch: EmptyAction{}, FalseBranch: EmptyAction{}} }
+func (z Zero) IsMultipleOfGrid() Bool { return True{TrueBranch: EmptyAction{}, FalseBranch: EmptyAction{}} }
+
+type Successor struct {
+	pred          Number
+	CompareTarget Number
+}
+
+func (s Successor) IdentifyClass()       {}
+func (s Successor) Class() string        { return "Successor" }
+func (s Successor) Next() Number         { return Successor{pred: s} }
+func (s Successor) CheckEquality() Bool  { return s.CompareTarget.CompareWithSuccessor() }
+func (s Successor) CompareWithZero() Bool { return False{TrueBranch: EmptyAction{}, FalseBranch: EmptyAction{}} }
+func (s Successor) CompareWithSuccessor() Bool { return Successor{pred: s.pred, CompareTarget: s.CombinePredecessors()}.CheckEquality() }
+func (s Successor) CombinePredecessors() Number { return s.CompareTarget.(Successor).pred }
+func (s Successor) IsMultipleOfGrid() Bool { return False{TrueBranch: EmptyAction{}, FalseBranch: EmptyAction{}} }
+
+type True struct{ TrueBranch, FalseBranch Action }
+
+func (t True) IdentifyClass()       {}
+func (t True) Class() string        { return "True" }
+func (t True) Select() Action       { return t.TrueBranch }
+
+type False struct{ TrueBranch, FalseBranch Action }
+
+func (f False) IdentifyClass()       {}
+func (f False) Class() string        { return "False" }
+func (f False) Select() Action       { return f.FalseBranch }
+
+type BranchFactory struct {
+	Condition   Bool
+	TrueBranch  Action
+	FalseBranch Action
+}
+
+type BoolResultContainer struct{ Value Bool }
+
+func (bf BranchFactory) Create() Bool {
+	container := &BoolResultContainer{}
+	TypeResolver{ClassName: bf.Condition.Class(), T: bf.TrueBranch, F: bf.FalseBranch, Target: container}.Resolve()
+	return container.Value
+}
+
+type TypeResolver struct {
+	ClassName string
+	T, F      Action
+	Target    *BoolResultContainer
+}
+
+func (tr TypeResolver) Resolve() {
+	tr.Target.Value = True{TrueBranch: tr.T, FalseBranch: tr.F}
+}
 
 type CameraStateHolder struct{ CurrentProjection ProjectionStrategy }
 
@@ -227,3 +316,111 @@ type SnapshotPoint[T Object] struct {
 	Color       T
 }
 func (sp SnapshotPoint[T]) IdentifyClass(consumer ClassConsumer) {}
+
+type Point3D struct {
+	X Number
+	Y Number
+	Z Number
+}
+
+type ProjectionStrategy interface {
+	Object
+	Project() Action
+	NextOrientation() ProjectionStrategy
+}
+
+type Vector2D struct {
+	U Number
+	V Number
+}
+
+type ProjectionAcceptor interface {
+	AcceptProjection() // Сигнатура пустая, вектор Vector2D будет лежать внутри структуры акцептора
+}
+
+type TopViewProjection struct {
+	Vertex       Point3D
+	Continuation ProjectionAcceptor
+}
+
+func (tvp TopViewProjection) IdentifyClass() {}
+func (tvp TopViewProjection) Project() Action {
+	// Создаем акцептор и инкапсулируем вычисленные данные внутрь него, параметры отсутствуют
+	tvp.Continuation.AcceptProjection() 
+	return EmptyAction{}
+}
+func (tvp TopViewProjection) NextOrientation() ProjectionStrategy { return SideViewProjection{} }
+
+type SideViewProjection struct {
+	Vertex       Point3D
+	Continuation ProjectionAcceptor
+}
+
+func (svp SideViewProjection) IdentifyClass() {}
+func (svp SideViewProjection) Project() Action {
+	svp.Continuation.AcceptProjection()
+	return EmptyAction{}
+}
+func (svp SideViewProjection) NextOrientation() ProjectionStrategy { return TopViewProjection{} }
+
+type GameColor interface{ Object }
+type SolidWhiteColor struct{}
+func (swc SolidWhiteColor) IdentifyClass() {}
+type GridLineColor struct{}
+func (glc GridLineColor) IdentifyClass() {}
+type Object3DColor struct{}
+func (o3c Object3DColor) IdentifyClass() {}
+type TransparentColor struct{}
+func (tc TransparentColor) IdentifyClass() {}
+
+type ColorAcceptor interface {
+	AcceptColor() // Сигнатура пустая
+}
+
+type SceneLayer interface {
+	Object
+	RenderPixel() Action
+}
+
+type WhiteBackgroundLayer struct{ Output ColorAcceptor }
+func (wbl WhiteBackgroundLayer) IdentifyClass() {}
+func (wbl WhiteBackgroundLayer) RenderPixel() Action {
+	wbl.Output.AcceptColor()
+	return EmptyAction{}
+}
+
+type CoordinateGridLayer struct {
+	CurrentStep Vector
+	Output      ColorAcceptor
+}
+func (cgl CoordinateGridLayer) IdentifyClass() {}
+func (cgl CoordinateGridLayer) RenderPixel() Action {
+	cgl.CurrentStep.IsGridIntersection().Select().Execute()
+	return EmptyAction{}
+}
+
+type ThreeDimensionalObjectLayer struct {
+	CurrentStep Vector
+	Output      ColorAcceptor
+}
+func (tdol ThreeDimensionalObjectLayer) IdentifyClass() {}
+func (tdol ThreeDimensionalObjectLayer) RenderPixel() Action {
+	tdol.CurrentStep.IsIntersecting3D().Select().Execute()
+	return EmptyAction{}
+}
+
+type Composited3DScene struct {
+	Background  SceneLayer
+	Grid        SceneLayer
+	Object3D    SceneLayer
+	FinalOutput ColorAcceptor
+}
+func (c3ds Composited3DScene) IdentifyClass() {}
+func (c3ds Composited3DScene) RenderPixel() Action {
+	c3ds.Object3D.RenderPixel()
+	return EmptyAction{}
+}
+
+type LayerAction struct{ Layer SceneLayer }
+func (la LayerAction) IdentifyClass() {}
+func (la LayerAction) Execute()       { la.Layer.RenderPixel() }
