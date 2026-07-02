b867df687f11f69d4584b96f1c23f86ae9582615
p33tuhovanton-byte <p33tuhov.anton@gmail.com>
2026-07-02 09:40:11 +0500

    Create scene.go

---
+++ example/scene.go
@@ -0,0 +1,108 @@
+package main
+
+type Point3D struct {
+	X Number
+	Y Number
+	Z Number
+}
+
+type ProjectionStrategy interface {
+	Object
+	Project() Action
+	NextOrientation() ProjectionStrategy
+}
+
+type Vector2D struct {
+	U Number
+	V Number
+}
+
+type ProjectionAcceptor interface {
+	AcceptProjection()
+}
+
+type TopViewProjection struct {
+	Vertex       Point3D
+	Continuation ProjectionAcceptor
+}
+
+func (tvp TopViewProjection) IdentifyClass() {}
+func (tvp TopViewProjection) Project() Action {
+	tvp.Continuation.AcceptProjection() 
+	return EmptyAction{}
+}
+func (tvp TopViewProjection) NextOrientation() ProjectionStrategy { return SideViewProjection{} }
+
+type SideViewProjection struct {
+	Vertex       Point3D
+	Continuation ProjectionAcceptor
+}
+
+func (svp SideViewProjection) IdentifyClass() {}
+func (svp SideViewProjection) Project() Action {
+	svp.Continuation.AcceptProjection()
+	return EmptyAction{}
+}
+func (svp SideViewProjection) NextOrientation() ProjectionStrategy { return TopViewProjection{} }
+
+type GameColor interface{ Object }
+type SolidWhiteColor struct{}
+func (swc SolidWhiteColor) IdentifyClass() {}
+type GridLineColor struct{}
+func (glc GridLineColor) IdentifyClass() {}
+type Object3DColor struct{}
+func (o3c Object3DColor) IdentifyClass() {}
+type TransparentColor struct{}
+func (tc TransparentColor) IdentifyClass() {}
+
+type ColorAcceptor interface {
+	AcceptColor()
+}
+
+type SceneLayer interface {
+	Object
+	RenderPixel() Action
+}
+
+type WhiteBackgroundLayer struct{ Output ColorAcceptor }
+func (wbl WhiteBackgroundLayer) IdentifyClass() {}
+func (wbl WhiteBackgroundLayer) RenderPixel() Action {
+	wbl.Output.AcceptColor()
+	return EmptyAction{}
+}
+
+type CoordinateGridLayer struct {
+	CurrentStep Vector
+	Output      ColorAcceptor
+}
+func (cgl CoordinateGridLayer) IdentifyClass() {}
+func (cgl CoordinateGridLayer) RenderPixel() Action {
+	cgl.CurrentStep.IsGridIntersection().Select().Execute()
+	return EmptyAction{}
+}
+
+type ThreeDimensionalObjectLayer struct {
+	CurrentStep Vector
+	Output      ColorAcceptor
+}
+func (tdol ThreeDimensionalObjectLayer) IdentifyClass() {}
+func (tdol ThreeDimensionalObjectLayer) RenderPixel() Action {
+	tdol.CurrentStep.IsIntersecting3D().Select().Execute()
+	return EmptyAction{}
+}
+
+type Composited3DScene struct {
+	Background  SceneLayer
+	Grid        SceneLayer
+	Object3D    SceneLayer
+	FinalOutput ColorAcceptor
+}
+func (c3ds Composited3DScene) IdentifyClass() {}
+func (c3ds Composited3DScene) RenderPixel() Action {
+	c3ds.Object3D.RenderPixel()
+	return EmptyAction{}
+}
+
+type LayerAction struct{ Layer SceneLayer }
+func (la LayerAction) IdentifyClass() {}
+func (la LayerAction) Execute()       { la.Layer.RenderPixel() }
